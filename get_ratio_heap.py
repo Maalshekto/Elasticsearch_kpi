524c90981d587c963254bccb9fba0201b0872403
maalshekto <tmartin@live.fr>
2019-03-01 11:23:11 +0100

    Factorization of get_ratio_heap.py

--- get_ratio_heap.py
+++ get_ratio_heap.py
@@ -7,10 +7,8 @@
 
 from elasticsearch import Elasticsearch, AuthenticationException
 import humanfriendly
-import os
-import sys
 import json
-import socket
+from utils.elasticsearch_utils import print_ko_message, socket_level_test, get_elasticsearch_params
 
 #     ______ ____   _   __ _____ ______ ___     _   __ ______ _____
 #    / ____// __ \ / | / // ___//_  __//   |   / | / //_  __// ___/
@@ -23,17 +21,12 @@ TAG_HEAP_USAGE = 'heap.current'
 TAG_HEAP_MAX = 'heap.max'
 TAG_RAM_MAX = 'ram.max'
 TAG_DISK_USAGE = 'disk.indices'
-TAG_ROLE = 'node.role'
 TAG_NAME = 'name'
 TAG_RATIO_CUR_HEAP_DISK_INDICES = 'ratio.cur_heap_disk_indices'
 TAG_RATIO_MAX_HEAP_DISK_INDICES = 'ratio.max_heap_disk_indices'
 TAG_NAME_ALLOC = 'node'
+TEST_ERROR_NAME = 'get_ratio_heap'
 
-SYSTEM_RETURN_CODE_ERROR = 0
-
-TIMEOUT_TEST_SOCKET_LEVEL = 3
-
-FLAG_DATA_ROLE = 'd'
 
 #      __  __ ______ __     ____   ______ ____       ______ __  __ _   __ ______ ______ ____ ____   _   __ _____
 #     / / / // ____// /    / __ \ / ____// __ \     / ____// / / // | / // ____//_  __//  _// __ \ / | / // ___/
@@ -41,27 +34,6 @@ FLAG_DATA_ROLE = 'd'
 #   / __  // /___ / /___ / ____// /___ / _, _/    / __/  / /_/ // /|  // /___   / /  _/ / / /_/ // /|  / ___/ /
 #  /_/ /_//_____//_____//_/    /_____//_/ |_|    /_/     \____//_/ |_/ \____/  /_/  /___/ \____//_/ |_/ /____/
 #
-
-
-def print_ko_message(message, exception = None):
-    """Print error messages in JSON format indicating cause.
-
-    Keyword arguments:
-        message -- The custom error message.
-        exception -- if the error is due to exception (default None)
-    """
-
-    if exception:
-        if hasattr(exception, 'message'):
-            message += exception.message
-        else:
-            message += str(exception)
-
-    error_message =  { "message" : "KO", "cause" : message, "name" : "get_ratio_heap"  }
-
-
-    print(json.dumps(error_message))
-    sys.exit(SYSTEM_RETURN_CODE_ERROR)
 
 
 def get_value_from_hr_tag(hr_value, tag):
@@ -75,7 +47,7 @@ def get_value_from_hr_tag(hr_value, tag):
 
     res = humanfriendly.parse_size(hr_value[tag], binary = True)
     return res if res else 0
-	
+    
 def get_value_from_tag(data, tag):
     """Retrieve a element of a specified tag from data.
 
@@ -85,30 +57,7 @@ def get_value_from_tag(data, tag):
     """
     res = data[tag]
     return int(res) if res else 0
-	
-def is_data_node(data):
-    """test for knowing if a node has a data role according
-       to nodes data.
-
-        Keyword arguments:
-            data -- The nodes data with 'node.role' field.
-    """
-    role = data[TAG_ROLE]
-    return FLAG_DATA_ROLE in role
-
-def socket_level_test(host, port):
-    """test for knowing if a node has a data role according
-           to nodes data.
-
-            Keyword arguments:
-                data -- The nodes data with 'node.role' field.
-    """
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(TIMEOUT_TEST_SOCKET_LEVEL)
-    res = sock.connect_ex((host, port))
-    return res == 0
-
-
+    
 
 
 #      __  ___ ___     ____ _   __     _____  ______ ____   ____ ____  ______
@@ -118,44 +67,7 @@ def socket_level_test(host, port):
 #  /_/  /_//_/  |_|/___//_/ |_/     /____/ \____//_/ |_|/___//_/     /_/
 #
 
-
-try:
-    # Retrieve inputs from ES_PARAMS or multiples environment variables.
-    es_params = os.getenv('ES_PARAMS') or ""
-    if es_params is "":
-        es_param = dict()
-
-        # Optional inputs
-        es_user = os.getenv('ES_USER') or 'elastic'
-        es_scheme = os.getenv('ES_SCHEME') or 'https'
-        es_capath = os.getenv('ES_PATH') or ""
-
-        es_param["port"] = int(os.environ['ES_PORT'])
-        es_param["host"] = os.environ['ES_HOST']
-        es_param["http_auth"] = [es_user, os.environ['ES_PWD']]
-        es_param["scheme"] = es_scheme
-        if es_capath != "":
-            es_param["ca_certs"] = es_capath
-
-        es_params = json.dumps([es_param])
-
-    json_acceptable_string = es_params.replace("'", "\"")
-    es_params = json.loads(json_acceptable_string)
-
-    # hosts reachable control:
-    for param in es_params:
-        if not socket_level_test(param['host'], param['port']):
-            print_ko_message(param['host'] + ":" + str(param['port']) + " not reachable - Check port.")
-
-except socket.gaierror:
-    print_ko_message("Host not reachable - Check host.")
-except ValueError as ve:
-    print_ko_message('Invalid input : ', ve)
-except KeyError as ke:
-    print_ko_message('Variable not set : ', ke)
-except Exception as e:
-    print_ko_message('Generic error : ', e)
-
+es_params = get_elasticsearch_params(TEST_ERROR_NAME)
 
 try:
     # Retrieve data from Elasticsearch.
@@ -164,9 +76,9 @@ try:
     alloc = es.cat.allocation(h=[TAG_DISK_USAGE, TAG_NAME_ALLOC], format='json', bytes="b")
 
 except AuthenticationException as ae:
-    print_ko_message('Invalid password or login')
+    print_ko_message('Invalid password or login', TEST_ERROR_NAME)
 except Exception as e:
-    print_ko_message('Generic error : ', e)
+    print_ko_message('Generic error : ', TEST_ERROR_NAME, e)
 
 
 # Ratio calculation and JSON output creation.
@@ -190,7 +102,7 @@ for node in memory_infos:
         heap_max = get_value_from_hr_tag(node, TAG_HEAP_MAX)
         heap_max_total += heap_max
         if disk_usage == 0:
-            print_ko_message('No data on node : ' + node[TAG_NAME])
+            print_ko_message('No data on node : ' + node[TAG_NAME], TEST_ERROR_NAME)
 
         json_result =  {"message" : "OK",
                         "value" : float(heap_usage)/float(disk_usage),
