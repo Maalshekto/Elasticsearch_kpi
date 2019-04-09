c32999473e23eb7b8289ce0d0278bf32f2f8e65b
maalshekto <tmartin@live.fr>
2019-04-09 16:53:36 +0200

    fix it

--- utils/elasticsearch_utils.py
+++ utils/elasticsearch_utils.py
@@ -18,7 +18,7 @@ def output_message_stdout(message):
 
 def output_message_kafka(message):
     producer = KafkaProducer(bootstrap_servers="172.22.0.1:9092", batch_size=1)
-    producer.send("metrics", message.encode())
+    producer.send("metrics", json.dumps(message).encode())
 
 
 def output_message(message):
